4ad40890169a30d0b5be200859d031578ccf0d66
STAN\stan <stanislav.emshanov@gmail.com>
2021-03-13 19:00:16 +0400

    add structure, edit webpack config

---
+++ src/components/App.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+export const App: React.FC = () => (
+  <div>
+    Hello world
+    <Button />
+  </div>
+);
---
+++ src/components/ui/button/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+import styles from "./style.module.css";
+
+export const Button: React.FC = () => (
+  <button className={styles.button}>НАЖМИ</button>
+);
